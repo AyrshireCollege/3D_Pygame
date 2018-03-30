393f42a21cd6b3f57172840d6d19cfdc070e1340
Gordon Stirling <gordon.stirling@ayrshire.ac.uk>
2018-03-30 09:12:54 +0100

    Tutorial 2 - Colouring the surfaces of the cube

--- main.py
+++ main.py
@@ -26,11 +26,44 @@ edges = (
     (6, 7),
     (5, 1),
     (5, 4),
-    (5, 7),
+    (5, 7)
+)
+
+colours = (
+    (1, 0, 0),
+    (0, 1, 0),
+    (0, 0, 1),
+    (0, 1, 0),
+    (1, 1, 1),
+    (0, 1, 1),
+    (1, 0, 0),
+    (0, 1, 0),
+    (0, 0, 1),
+    (1, 0, 0),
+    (1, 1, 1),
+    (0, 1, 1)
+)
+
+surfaces = (
+    (0, 1, 2, 3),
+    (3, 2, 7, 6),
+    (6, 7, 5, 4),
+    (4, 5, 1, 0),
+    (1, 5, 7, 2),
+    (4, 0, 3, 6)
 )
 
 
 def Cube():
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        x = 0
+        for vertex in surface:
+            x += 1
+            glColor3fv(colours[x])
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
