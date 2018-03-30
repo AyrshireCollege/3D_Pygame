40c1a256814cb284b9963ee7525de29dac23de3c
Gordon Stirling <gordon.stirling@ayrshire.ac.uk>
2018-03-30 08:55:33 +0100

    Tutorial 1 - Intro to OpenGL and create a rotating cube

--- main.py
+++ main.py
@@ -1,2 +1,63 @@
 import pygame
-import OpenGL
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+vertices = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+)
+
+edges = (
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7),
+)
+
+
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex(vertices[vertex])
+    glEnd()
+
+
+def main():
+    pygame.init()
+    screen = (800, 600)
+    pygame.display.set_mode(screen, DOUBLEBUF | OPENGL)
+
+    gluPerspective(45, (screen[0]/screen[1]), 0.1, 50.0)
+
+    glTranslate(0.0, 0.0, -5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+main()
